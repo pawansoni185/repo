4f4de0a121d4fef21c8ebacae52761bfd3beb117
pawan.soni <pawan.soni@YI1004091LT.Yash.com>
2018-06-06 16:44:42 +0530

    changes in controller

--- SpringMVCHibernateCRUD/src/main/java/com/jwt/controller/EmployeeController.java
+++ SpringMVCHibernateCRUD/src/main/java/com/jwt/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import com.jwt.service.EmployeeService;
 @Controller
 public class EmployeeController {
 
-	private static final Logger logger = Logger
+	private static   final Logger logger = Logger
 			.getLogger(EmployeeController.class);
 
 	public EmployeeController() {
